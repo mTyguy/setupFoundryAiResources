d2405991707eed1b7786c0b1cc27dadf23791b42
ty <tmarinoff@icloud.com>
2026-05-11 16:09:01 -0400

    improvements

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 buildAiEnv.ps1 -- Script deploys necessary Foundry resources to create an agent that is capable of doing web searches.
 
 agentConversation.ps1 -- script that allows you to chat with the bot that has a conversation history and remembers
+
+TO DO:
+1) Assign Least Privileges to creator to the Resource Group to be able to perform foundry tasks. -Done-
+2) Allow creator to give access to agent to other users based on UPN so other people can use the agent. Entra group or individual users. -Done-
+3) Allow creator to specify the Instructions for the agent. -Done-
+4) Give creator more options for what model they would like to use.
+
+5) let Creator define the capabilities/tools of the agent
+    a) Websearch
+    b) file search
+    c) foundryIQ connection
+    d) other connections?
+
+6) ???
+
+Other Things:
+a) More error recording
+b) Diagnostics exporting
+
+
--- buildAiEnv.ps1
+++ buildAiEnv.ps1
@@ -4,6 +4,8 @@ Model is gpt-4.1
 #>
 Write-Host "Make sure you are logged into Az PowerShell first!!!" -Foregroundcolor Red
 
+$scriptRunner = (az account show | ConvertFrom-Json)
+
 $rgGroupName = Read-Host "Enter the name of the resource group you want to create"
 $location = Read-Host "Enter the region you want the resources to reside in, example: eastus"
 $foundryResourceName = Read-Host "Enter the name for your Foundry resource"
@@ -19,7 +21,7 @@ if ($rgGroupExist -eq $false) {
 
   if ($createRgResponse.properties.provisioningState -eq "Succeeded") {
     Write-Host "$($createRgResponse.name) created" -Foregroundcolor DarkCyan 
-    Write-Host "resouce id = $($createRgResponse.id)" -Foregroundcolor DarkCyan
+    Write-Host "Resource id = $($createRgResponse.id)" -Foregroundcolor DarkCyan
   } else {
       Write-Host "Resource was not created... exiting" -Foregroundcolor DarkRed
       exit
@@ -28,6 +30,23 @@ if ($rgGroupExist -eq $false) {
 } elseif ($rgGroupExist -eq $true) {
     Write-Host "$rgGroupName already exists... continuing" -Foregroundcolor Yellow
   }
+Write-Host "####################"
+
+
+# Give appropriate Azure role
+Write-Host "Would you like to give yourself Azure AI Developer Permissions (1) or Azure AI User Permissions? (2)" -Foregroundcolor DarkCyan
+Write-Host "Azure AI Developer - Can perform all actions within an Azure AI resource besides managing the resource itself. Applies to Azure Machine Learning and Foundry hubs only." -Foregroundcolor Yellow
+Write-Host "Azure AI User - Grants reader access to AI projects, reader access to AI accounts, and data actions for an AI project." -Foregroundcolor Yellow
+$creatorRoleSelection = Read-Host "1 or 2"
+
+if ($creatorRoleSelection -eq "1"){
+  $creatorRoleAiDeveloper = az role assignment create --assignee "$($scriptRunner.user.name)" --role "Azure AI Developer" --scope "$($createRgResponse.id)"
+
+  } elseif ($creatorRoleSelection -eq "2") {
+    $creatorRoleAiUSer = az role assignment create --assignee "$($scriptRunner.user.name)" --role "Azure AI User" --scope "$($createRgResponse.id)"
+}
+Write-Host "####################"
+
 
 # Check if Foundry resource already exists
 $foundryResourceExist = (az cognitiveservices account list -g $rgGroupName | ConvertFrom-Json)
@@ -51,13 +70,13 @@ if ($foundryResourceExist.name -ne $foundryResourceName) {
     Write-Host "$foundryResourceName already exists... continuing" -Foregroundcolor Yellow
   }
 
-#
-# Check if custom subdomain is null or not
-#if ($foundryCreateResponse.properties.customSubDomainName -eq $null) {
+Write-Host "####################"
 
-  Write-Host "Adding subdomain $foundryDomainName... this can take a few moments"
+Write-Host "Adding subdomain $foundryDomainName... this can take a few moments"
 
-  $foundryDomainNameResponse = (($foundryDomainCreate = az cognitiveservices account update --name $foundryResourceName --resource-group $rgGroupName --custom-domain $foundryDomainName) | ConvertFrom-Json)
+$foundryDomainNameResponse = (($foundryDomainCreate = az cognitiveservices account update --name $foundryResourceName --resource-group $rgGroupName --custom-domain $foundryDomainName) | ConvertFrom-Json)
+
+Write-Host "####################"
 
 # Create Project
 Write-Host "Creating $foundryProjectName... this can take a few moments"
@@ -72,6 +91,10 @@ if ($foundryProjectCreateResponse.properties.provisioningState -eq "Succeeded") 
     exit
   }
 
+Write-Host "####################"
+
+Write-Host "Select what model to use -- placeholder" -Foregroundcolor Red
+
 $deployModel = (az cognitiveservices account deployment create --name $foundryResourceName --resource-group $rgGroupName --deployment-name gpt-4.1 --model-name gpt-4.1 --model-version 2025-04-14 --model-format OpenAI --sku-capacity 1 --sku-name Standard | ConvertFrom-Json)
 
 if ($deployModel.properties.provisioningState -eq "Succeeded") {
@@ -84,7 +107,8 @@ if ($deployModel.properties.provisioningState -eq "Succeeded") {
 Write-Host "environment setup!" -Foregroundcolor DarkCyan
 
 # Deploy Agent
-$agentName = Read-Host "enter agent's name"
+$agentName = Read-Host "Enter agent's name"
+$agentInstructions = Read-Host "Enter the Agent's instruction set... example: You are a helpful assistant that answers general questions"
 
 $azureAiToken = (az account get-access-token --resource https://ai.azure.com/ --query accessToken -o tsv)
 
@@ -97,7 +121,7 @@ $Body = @"
    "name": "$agentName",
    "definition": {
       "kind": "prompt",
-      "instructions": "You are a helpful assistant that answers general questions",
+      "instructions": "$agentInstructions",
       "model": "gpt-4.1",
       "tools": [
         { "type": "web_search" }
@@ -112,8 +136,31 @@ $response = Invoke-WebRequest -Method Post -Uri $Uri -Headers $Headers -Body $Bo
 
 if ($response.StatusCode -eq "200") {
   Write-Host "agent successfully deployed" -Foregroundcolor DarkCyan
-  Write-Host "view your resources at https://ai.azure.com"
-  Write-Host "make sure you have at least Azure AI Owner permissions on $($rgGroupName)" -Foregroundcolor Yellow
+  Write-Host "view your resources at https://ai.azure.com !"
 } else {
    Write-Host "deployment of agent errored" -Foregroundcolor Red
   }
+
+Write-Host "####################"
+
+Write-Host "Resources Deployed!"
+
+$continueWithIam = Read-Host "Would you like to grant others access to this resource while you are here? y/n"
+Write-Host "This script assumes you have User Access Administrator role or similar permissions" -Foregroundcolor Red
+
+if ($continueWithIAM -eq "y") {
+  Write-Host "This script assumes you have User Access Administrator role or similar permissions" -Foregroundcolor Red
+  do {
+    $userToAdd = Read-Host "Who would you like to add to resource? UPN"
+    Write-Host "Use Ctrl C to exit loop" -Foregroundcolor Yellow
+    $userToAddPerm = Read-Host "Azure AI Developer (1) or Azure AI User (2)?"
+      if ($userToAddPerm -eq "1") {$userToAddPerm = "Azure AI Developer"} elseif ($userToAddPerm -eq "2") {$userToAddPerm = "Azure AI User"}
+    
+    $addUserAIM = az role assignment create --assignee "$userToAdd" --role "$userToAddPerm" --scope "$($createRgResponse.id)"
+
+    } while ($true)
+
+  } else {
+    Write-Host "exiting script..."
+    exit
+}
